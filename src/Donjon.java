94203a31e9f82e229386165ac4cc1e2c91532279
Fella Bennounas <bennounas@unistra.fr>
2025-05-15 13:37:32 +0200

    ajout des tests donjon

--- src/Donjon.java
+++ src/Donjon.java
@@ -10,6 +10,8 @@ import personnages.Entite;
 
 public class Donjon {
     private int m_num;
+    private int m_nb_monstres;
+    private int m_nb_personnages;
     private int m_longueur;
     private int m_largeur;
     private Hashtable<Integer, Entite> m_entites;
@@ -23,16 +25,59 @@ public class Donjon {
         m_cases = new Hashtable<>(longueur * largeur);
     }
 
-    // Ajouter une entité (personnage ou monstre)
+    // Ajouter une entité (personnage ou monstre) avec un id
     public void ajoutEntite(Entite e) {
         int idEntite = m_entites.size() + 1;
-        m_entites.put(idEntite, e);
+        m_entites.put(idEntite, e);//ajout de l'entite dans notre tableau d'entites
+
+        if (e instanceof Monstre) m_nb_monstres++;
+        else if (e instanceof Personnage) m_nb_personnages++;
     }
 
+    public void afficherEntites() {
+        System.out.println("Nous avons ajouté à la partie :");
+        for (Map.Entry<Integer, Entite> entry : getEntites().entrySet()) {
+            System.out.println(entry.getValue().getNom());
+        }
+    }
+
+
+
+    public int getNombreEntites() {
+        return m_entites.size();
+    }
+
+    public Hashtable<Integer, Entite> getEntites() {
+        return m_entites;
+    }
+
+
+
+    //methode qui nommerra toutes les cases du plateau
+    public void nommerCases() {
+        for (int i = 0; i < m_longueur; i++) {
+            for (int j = 0; j < m_largeur; j++) {
+                char lettre = (char) ('A' + j); // parcours l'alphabet
+                String nomCase = "" + lettre + i; // concatenation
+                m_cases.put(nomCase, new int[]{0, 0}); //la ligne du dico a un nom et les tabs pour le moment rien dessus
+            }
+        }
+    }
+
+
+
+    public Hashtable<String, int[]> getCases() {
+        return m_cases;
+    }
+
+
+
     // Ordre de jeu (en fonction des scores des dés)
-    public void ordreDeJeu() {
+    public StringBuilder ordreDeJeu() {
         Random random = new Random();
         Hashtable<Integer, Integer> scores = new Hashtable<>();
+
+        StringBuilder retourne = new StringBuilder();
 
         for (Map.Entry<Integer, Entite> entry : m_entites.entrySet()) {
             int id = entry.getKey();
@@ -41,31 +86,32 @@ public class Donjon {
             int score = 1 + random.nextInt(20);
             scores.put(id, score);
 
-            System.out.println(e.getNom() + " a obtenu : " + score);
         }
 
         ArrayList<Integer> ids = new ArrayList<>(m_entites.keySet());
         ids.sort((id1, id2) -> Integer.compare(scores.get(id2), scores.get(id1)));
 
-        System.out.println("\nOrdre de jeu :");
+        retourne.append("\nOrdre de jeu :");
         for (int id : ids) {
-            System.out.println("- " + m_entites.get(id).getNom());
+            retourne.append("- " + m_entites.get(id).getNom());
         }
+
+        return retourne;
     }
 
     // Déplacement d'une entité
-    public void seDeplacer(int entitéID, String direction) {
-        Entite e = m_entites.get(entitéID);
+    public String seDeplacer(int entiteID, String direction) {
+        Entite e = m_entites.get(entiteID);
+        String retourne;
         if (e == null) {
-            System.out.println("Aucune entité avec cet ID.");
-            return;
+            return "Aucune entité avec cet ID.";
         }
 
         for (Map.Entry<String, int[]> entry : m_cases.entrySet()) {
             String nomCase = entry.getKey();
             int[] valeurs = entry.getValue();
 
-            if (valeurs.length > 0 && valeurs[0] == entitéID) {
+            if (valeurs.length > 0 && valeurs[0] == entiteID) {
                 char lettre = nomCase.charAt(0); // colonne
                 int numero = Integer.parseInt(nomCase.substring(1)); // ligne
 
@@ -80,8 +126,7 @@ public class Donjon {
                     case "diagonale bas gauche": lettre -= 1; numero += 1; break;
                     case "diagonale bas droite": lettre += 1; numero += 1; break;
                     default:
-                        System.out.println("Direction invalide");
-                        return;
+                        return "Direction invalide";
                 }
 
                 String nouvelleCase = "" + lettre + numero;
@@ -91,93 +136,128 @@ public class Donjon {
                     if (destination.length == 0 || destination[0] == 0) {
                         m_cases.put(nouvelleCase, valeurs); // déplacer
                         m_cases.put(nomCase, new int[]{0}); // libérer l'ancienne case
-                        System.out.println(e.getNom() + " se déplace vers " + nouvelleCase);
+                        retourne=(e.getNom() + " se déplace vers " + nouvelleCase);
                     } else {
-                        System.out.println("Case occupée !");
+                        retourne=("Case occupée !");
                     }
                 } else {
-                    System.out.println("Case inexistante !");
+                    retourne=("Case inexistante !");
                 }
 
-                return;
+                return retourne;
             }
         }
 
-        System.out.println("Entité non trouvée dans le donjon.");
+        return ("Entité non trouvée dans le donjon.");
     }
+
+
+    public String finDonjon(Personnage perso) {
+        if (!perso.estVivant()) {
+            return (perso.getNom()+" est mort, votre équipe a échoué le Donjon "+m_num+"...");
+        }
+        if (m_nb_monstres==0) {
+            return ("Victoire! donjon : " +m_num+" terminé \n"+"Félicitation! tous les monstres on été vaincus, et l'équipe est au complet. Á bientot dans un nouveau Donjon :)");
+        }
+        return "l'equipe est au complet: "+m_nb_personnages+"\n"+"il reste encore "+m_nb_monstres+" à vaincre, courage!";
+
+    }
+
+
+
 
     // Affichage du plateau
     public void Affichage_plateau() {
-        char[][] plateau = new char[m_longueur][m_largeur];
+        int tour = 1;
+        String[][] plateau = new String[m_longueur][m_largeur];
 
-        // Initialiser le plateau avec des points (cases vides)
+        //affichage entete du plateau
+        System.out.println("Donjon "+m_num+":\n");
+        if (tour>m_entites.size()) {
+            System.out.println("tour "+tour+"\t\t"+m_entites.get(tour- (m_entites.size()) ).getNom()); //dans le tableau des personnages (dans l'ordre, on prend le nom du perso avec l'id
+            System.out.println("À toi de jouer!");
+
+        }
+        else {
+            System.out.println("tour "+tour+"\t\t"+m_entites.get(tour).getNom());
+            System.out.println("À toi de jouer!");
+        }
+
+        // Initialiser toutes les cases à '.'
         for (int i = 0; i < m_longueur; i++) {
             for (int j = 0; j < m_largeur; j++) {
-                plateau[i][j] = '.'; // '.' représente une case vide
+                plateau[i][j] = ".";
             }
         }
 
-        // Affichage des entités sur le plateau
-        for (Map.Entry<Integer, Entite> entry : m_entites.entrySet()) { //pour chaque perso/monstre
+        // Placer les entités (personnages ou monstres)
+        for (Map.Entry<Integer, Entite> entry : m_entites.entrySet()) {
             int id = entry.getKey();
             Entite e = entry.getValue();
 
-            for (Map.Entry<String, int[]> caseEntry : m_cases.entrySet()) { //on parcours tout le plateau
-                String caseNom = caseEntry.getKey(); //le nom de la case A2
-                int[] caseValeurs = caseEntry.getValue(); //le tableau a deux cases de cette case
+            for (Map.Entry<String, int[]> caseEntry : m_cases.entrySet()) {
+                int[] valeurs = caseEntry.getValue();
+                if (valeurs[0] == id) {
+                    String caseNom = caseEntry.getKey();
+                    int x = caseNom.charAt(0) - 'A';
+                    int y = Integer.parseInt(caseNom.substring(1)) - 1;
 
-                if (caseValeurs[0] == id) {//si la premiere case du tab a la meme valeur qur l'id
-                    int x = caseNom.charAt(0) - 'A'; // Calculer la colonne (en fonction du nom de la case)
-                    int y = Integer.parseInt(caseNom.substring(1)) - 1; // Calculer la ligne
-
-                    // Si c'est un personnage, on affiche son ID
                     if (e instanceof Personnage) {
-                        plateau[y][x] = (char) ('0' + id); // Affichage de l'ID du personnage
-                    }
-                    // Si c'est un monstre, on affiche l'ID et un "M"
-                    else if (e instanceof Monstre) {
-                        plateau[y][x] = (char) ('0' + id); // Affichage de l'ID du monstre
-                        plateau[y][x]=+'M'; // Ajout d'un "M" pour indiquer que c'est un monstre
+                        plateau[y][x] = (String) ("" + id);
+                    } else if (e instanceof Monstre) {
+                        plateau[y][x] = (String) (("" + id));
+                        plateau[y][x] += 'M';
                     }
                 }
             }
         }
 
-        // Gestion des obstacles et des équipements
-        for (int i = 0; i < m_longueur; i++) {
-            for (int j = 0; j < m_largeur; j++) { // Parcours de la grille
-                String caseNom = "" + (char) ('A' + j) + (i + 1); // Nom de la case
-                char currentChar = plateau[i][j]; // Valeur actuelle de la case
+        // Placer les obstacles et équipements
+        for (Map.Entry<String, int[]> caseEntry : m_cases.entrySet()) {
+            String caseNom = caseEntry.getKey();
+            int[] valeurs = caseEntry.getValue();
+            int x = caseNom.charAt(0) - 'A';
+            int y = Integer.parseInt(caseNom.substring(1)) - 1;
 
-                // Vérifier si l'ID dans la première position du tableau m_cases est supérieur à la taille de m_entites
-                if (m_cases.containsKey(caseNom)) {
-                    int[] caseValeurs = m_cases.get(caseNom);
+            if (valeurs[0] > m_entites.size()) {
+                plateau[y][x] = "x";
+            }
 
-                    // Si l'ID est supérieur à la taille de m_entites, afficher un obstacle (x)
-                    if (caseValeurs.length > 0 && caseValeurs[0] > m_entites.size()) {
-                        plateau[i][j] = 'x'; // Afficher un 'x' pour un obstacle
-                    }
-
-                    // Vérifier si le deuxième élément du tableau est différent de 0 (il y'a equipement)
-                    if (caseValeurs[1] != 0) {
-                        if (currentChar == '.') {
-                            plateau[i][j] = '*'; // Remplacer le point par un '*' si la case est occupée
-                        }
-                        else {
-                            plateau[i][j] = (char) (currentChar + '*'); // Ajouter '*' à ce qui existe déjà
-                        }
-                    }
+            if (valeurs[1] != 0) {
+                // Ajout d’un * si équipement
+                if (plateau[y][x] == ".") {
+                    plateau[y][x] = "*";
+                } else {
+                    // fusionner l’équipement avec un autre élément
+                    plateau[y][x] += "*";
                 }
             }
         }
 
-        // Affichage final du plateau
+        // Affichage en-tête colonne A B C ...
+        System.out.print("   ");
+        for (int j = 0; j < m_largeur; j++) {
+            System.out.print(" " + (char) ('A' + j));
+        }
+        System.out.println();
+
+        // Affichage des lignes du plateau
         for (int i = 0; i < m_longueur; i++) {
+            System.out.printf("%2d ", i + 1); // numéro de ligne
             for (int j = 0; j < m_largeur; j++) {
-                System.out.print(plateau[i][j] + " ");
+                System.out.print(" " + plateau[i][j]);
             }
             System.out.println();
         }
     }
 
-}
+    public void Affichage_ordre() {
+        System.out.println(ordreDeJeu());
+    }
+
+    public void Affichage_deplacement(int entiteID, String direction) {
+        System.out.println(seDeplacer(entiteID, direction));
+    }
+
+
+    }
--- tests/DonjonTest.java
+++ tests/DonjonTest.java
@@ -1,6 +1,8 @@
 import personnages.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DonjonTest {
@@ -9,33 +11,86 @@ class DonjonTest {
     void ajoutEntite() {
         Donjon donjon = new Donjon(1, 5, 7);
 
+        Entite p1 = new Guerrier("Fella", Race.ELFE);
+        Entite p2 = new Guerrier("Elora", Race.HUMAIN);
+        Entite p3 = new Guerrier("Jomni", Race.ELFE);
+        Entite p4 =new Monstre(1,"Dragon",20,24,6,9,45,87,34,3,6);
 
-        Entite perso = new Clerc("Jojo", Race.HUMAIN);
+        donjon.ajoutEntite(p1);
+        donjon.ajoutEntite(p2);
+        donjon.ajoutEntite(p3);
+        donjon.ajoutEntite(p4);
 
-        // Ajout de l'entité
-        donjon.ajoutEntite(perso);
-
-        // Vérification que le donjon contient maintenant 1 entité
-        // Attention : ici on ne peut pas accéder directement à `m_entites` car il est `private`
-        // donc on va tricher un peu en réutilisant la méthode ordreDeJeu() qui affiche tous les noms
-
-        // Tu peux aussi ajouter une méthode dans Donjon pour connaître le nombre d'entités
-        // Par exemple :
-        // public int getNombreEntites() { return m_entites.size(); }
-
-        assertEquals(1, donjon.getNombreEntites()); // Test si une entité a bien été ajoutée
+          donjon.afficherEntites();
     }
 
-
-@org.junit.jupiter.api.Test
+    @Test
     void ordreDeJeu() {
+        Donjon donjon = new Donjon(1, 5, 5);
+
+        Entite p1 = new Guerrier("Fella", Race.ELFE);
+        Entite p2 = new Guerrier("Elora", Race.HUMAIN);
+        Entite p3 = new Guerrier("Jomni", Race.ELFE);
+        Entite p4 =new Monstre(1,"Dragon",20,24,6,9,45,87,34,3,6);
+
+        donjon.ajoutEntite(p1);
+        donjon.ajoutEntite(p2);
+        donjon.ajoutEntite(p3);
+        donjon.ajoutEntite(p4);
+
+        String ordre = donjon.ordreDeJeu().toString();
+       System.out.println(ordre);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void seDeplacer() {
+        Donjon donjon = new Donjon(1, 5, 5);
+
+        donjon.nommerCases();
+
+        Entite p1 = new Guerrier("Fella", Race.NAIN);
+        Entite p2 = new Clerc("Elora", Race.NAIN);
+        Entite p3 =new Monstre(1,"Dragon",20,24,6,9,45,87,34,3,6);
+        donjon.ajoutEntite(p1);
+        donjon.ajoutEntite(p2);
+        donjon.ajoutEntite(p3);
+
+        donjon.afficherEntites();
+
+        // On place manuellement l'entité dans une case
+        donjon.getCases().put("A1", new int[]{1, 0}); // ID 1, pas d'equipement sur cette case
+        donjon.getCases().put("B4", new int[]{1, 0});
+        donjon.getCases().put("E5", new int[]{1, 0});
+
+        String message1 = donjon.seDeplacer(1, "bas");
+        String message2 = donjon.seDeplacer(2, "uiugyug");
+        String message3 = donjon.seDeplacer(3, "bas");
+
+        System.out.println(message1);
+        System.out.println(message2);
+        System.out.println(message3);
+
     }
 
-    @org.junit.jupiter.api.Test
+
+
+    @Test
     void affichage_plateau() {
+        Donjon donjon = new Donjon(1, 7, 10);
+
+        Entite p1 = new Magicien("Fella", Race.HUMAIN);
+        Entite p2 = new Guerrier("Elora", Race.ELFE);
+        Entite p3 = new Monstre(1,"Dragon",20,24,6,9,45,87,34,3,6);
+        donjon.ajoutEntite(p1);
+        donjon.ajoutEntite(p2);
+        donjon.ajoutEntite(p3);
+
+        donjon.getCases().put("A5", new int[]{1, 0});
+        donjon.getCases().put("B4", new int[]{2, 1}); //2eme case tab pleine, donc equipement
+        donjon.getCases().put("D4", new int[]{3, 0});
+        donjon.getCases().put("J2", new int[]{3, 6});
+        donjon.getCases().put("E5", new int[]{4, 0}); //plus que le nombre d'id, donc ca sera un obstacle
+
+       donjon.Affichage_plateau();
     }
 }
